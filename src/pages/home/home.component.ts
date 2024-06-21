012212f7b8c38a6839031a230de312e7d372aa0f
Tiana-droid <adelusic@gmail.com>
2024-06-21 11:03:53 +0100

    rough work

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -4,9 +4,60 @@ import { Component } from '@angular/core';
   selector: 'app-home',
   standalone: true,
   imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  template: `
+    <main>
+      <section class="intro">
+        <small>Hi, my name</small>
+        <h1>Christianah Adelusi</h1>
+        <h2>A Frontend Developer</h2>
+      </section>
+      <section class="works">
+        <h1>Work Category</h1>
+        <div class="card">
+          @for (os of arr; track os.id) {
+              <div class="card-content">
+            <h3>{{ os.heading }}</h3>
+            <div class="project-image">
+              <a href="/works">
+                <img src="{{ os.image }}" alt="{{ os.tag }}" />
+              </a>
+            </div>
+          </div>
+            }
+
+        </div>
+      </section>
+    </main>
+  `,
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  title = 'Home | Angular - portfolio';
 
+  arr = [
+    {
+      id: 1,
+      heading: 'E-Commerce',
+      tag: 'e-commerce',
+      image: '../../assets/corperstrade.png',
+    },
+    {
+      id: 2,
+      heading: 'FinTech',
+      tag: 'fintech',
+      image: '../../assets/rackleblock.png',
+    },
+    {
+      id: 3,
+      heading: 'EduTech',
+      tag: 'edutech',
+      image: '../../assets/Screenshot.png',
+    },
+    {
+      id: 4,
+      heading: 'Portfolio websites',
+      tag: 'websites',
+      image: '../../assets/sojitade.png',
+    },
+  ];
 }
--- src/pages/works/works.component.ts
+++ src/pages/works/works.component.ts
@@ -5,9 +5,47 @@ import { Component } from '@angular/core';
   selector: 'app-works',
   standalone: true,
   imports:[],
-  templateUrl: './works.component.html',
+  template: `<section class="works">
+  <h1>Work Category</h1>
+  <div class="card">
+    @for (os of arr; track os.id) {
+        <div class="card-content">
+      <h3>{{ os.heading }}</h3>
+      <div class="project-image">
+        <img src="{{ os.image }}" alt="{{ os.tag }}" />
+      </div>
+    </div>
+      }
+
+  </div>
+</section>`,
   styleUrl: './works.component.scss'
 })
 export class WorksComponent {
-
+  arr = [
+    {
+      id: 1,
+      heading: 'E-Commerce',
+      tag: 'e-commerce',
+      image: '../../assets/corperstrade.png',
+    },
+    {
+      id: 2,
+      heading: 'FinTech',
+      tag: 'fintech',
+      image: '../../assets/rackleblock.png',
+    },
+    {
+      id: 3,
+      heading: 'EduTech',
+      tag: 'edutech',
+      image: '../../assets/Screenshot.png',
+    },
+    {
+      id: 4,
+      heading: 'Portfolio websites',
+      tag: 'websites',
+      image: '../../assets/sojitade.png',
+    },
+  ];
 }
